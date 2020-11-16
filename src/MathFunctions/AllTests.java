9043a5890698910342b0b1299d52cd67b167b502
Nikhil kothari <nikhilkothari003@gmail.com>
2020-11-17 01:37:26 +0530

    test case final version

---
+++ src/MathFunctions/AllTests.java
@@ -0,0 +1,19 @@
+package MathFunctions;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	TestCase_version_1.class,
+	TestCase_version_2.class,
+	TestCase_version_3.class,
+	TestCase_version_4.class
+})
+
+
+public class AllTests {
+}
